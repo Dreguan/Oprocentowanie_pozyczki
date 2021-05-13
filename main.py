2fa665dfb350d89fd3a37dd5108077468a824359
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-05-13 17:17:34 +0200

    Dodanie działania o ile mniej jest do spłacenia.

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ print("Podaj kwotę jednej raty: ")
 rata = int(input())
 stopa = float(1.592824484)
 pozostala_kwota = (1 + (( stopa + oprocentowanie ) / 1200 )) * kwota_pozyczki - rata
-print("Twoja pozostała kwota kredytu to {}.".format(pozostala_kwota))
+
+ile_mniej = kwota_pozyczki - pozostala_kwota
+
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiącu.".format(pozostala_kwota, ile_mniej))
+
+
