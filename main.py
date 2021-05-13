2dd9aa750ec2bf449f5769db3403a0f017dc9ba8
Marta Skibicka <marta.skibicka.contact@gmail.com>
2021-05-13 17:56:14 +0200

    Dodanie zaciągania danych z pliku stopy_procentowe.txt

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-print("Podaj kwotę początkową pożyczki: ")
 kwota_pozyczki = int(input())
-print("Podaj oprocentowanie: ")
+print("Kwota początkowa pożyczki: {}zł".format(kwota_pozyczki))
 oprocentowanie = int(input())
-print("Podaj kwotę jednej raty: ")
+print("Oprocentowanie: {}%".format(oprocentowanie))
 rata = int(input())
-stopa = float(1.592824484)
+print("Kwota jednej raty: {}zł".format(rata))
+stopa = float(input())
 pozostala_kwota = (1 + (( stopa + oprocentowanie ) / 1200 )) * kwota_pozyczki - rata
 ile_mniej = kwota_pozyczki - pozostala_kwota
 print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiącu.".format(pozostala_kwota, ile_mniej))
 
-stopa = float(-0.453509101)
+stopa = float(input())
 pozostala_kwota1 = (1 + ((stopa + oprocentowanie ) / 1200 )) * pozostala_kwota - rata
 ile_mniej =  pozostala_kwota - pozostala_kwota1
+pozostala_kwota = pozostala_kwota1
+print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiącu.".format(pozostala_kwota1, ile_mniej))
+
+stopa = float(input())
+pozostala_kwota1 = (1 + ((stopa + oprocentowanie ) / 1200 )) * pozostala_kwota - rata
+ile_mniej =  pozostala_kwota - pozostala_kwota1
+pozostala_kwota = pozostala_kwota1
 print("Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiącu.".format(pozostala_kwota1, ile_mniej))
 
